b0a5be150dd3b58c21a00c27f70a7792c5bbaf12
DaMaus <maus.escuela@gmail.com>
2016-03-22 23:16:02 -0600

    Aqui slo va el ejemplo de como usar la lista
    Hace falta idear una forma de cargar los archivos .txt a las listas

--- elm_uso_lista.c
+++ elm_uso_lista.c
@@ -1,37 +1,21 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "lista.h"
 #include "lista.c"
-
-void cargar(char depto[])  //esta funcion leera el documento de un departamento y guardara el contenido en una lista ligada
-{
-	FILE *f;
-	char caracter;
-	f = fopen(depto,"r");
-	if (f == NULL)
-			printf("\nError de apertura del archivo. \n\n");
-    else{
-		    printf("\nEl contenido del archivo de prueba es \n\n");
-		    while (feof(f) == 0)
-		    {
-				caracter = fgetc(f);
-				printf("%c",caracter);
-		    }
-        }
-    fclose(f);
-}
-
 int main(){
 	NodoInt *lista = NULL;
-	lista = listaInt_ingresar(lista,5,10);
-	lista = listaInt_ingresar(lista,5,8);
-	lista = listaInt_ingresar(lista,1,5);
-	lista = listaInt_remover(lista,1);
-	listaInt_imprimir(lista);
-	lista = listaInt_borrar(lista);
 
-	char depto[]="comida.txt";
-	cargar(depto);
+	lista = listaInt_ingresar(lista,5,5,"leche");
+	lista = listaInt_ingresar(lista,6,6,"pan");
+	lista = listaInt_ingresar(lista,7,7,"huevo");
+	lista = listaInt_ingresar(lista,8,8,"piña");
+	lista = listaInt_ingresar(lista,4,4,"pera");
+
+	lista = listaInt_ingresarEn(lista,4,4,"pera",3);
+
+	
+	listaInt_imprimir(lista);
 	return 0;
 }
