8bb69a8cedf50954b1f4868f63b6085b4e628d0e
MenesesCKevin <k3.22@live.com.mx>
2016-03-25 21:59:00 -0500

    Added files via upload

--- usolista.c
+++ usolista.c
@@ -10,7 +10,6 @@ NodoInt* leerArchivo(char dep[15],NodoInt *lista ){
 	f=fopen(dep,"rt");
 	if (f==0){
 		printf("Error\n");
-		system("pause");
 	}
 	int i,j,n=0;
 	char a[15],c;
@@ -30,11 +29,26 @@ void guardarArchivo(char dep[15],int cantidad,int costo,char nuevoP[15]){
 	f=fopen(dep,"at");
 	if (f==0){
 		printf("Error\n");
-		system("pause");
 	}
 	fprintf( f, "\n%s\t%d\t%d",nuevoP,costo,cantidad);
 	fclose(f);
 }
+
+void reescribeArchivo(char dep[15],NodoInt *inicio)
+{
+	NodoInt *nodoActual = inicio;
+ 	FILE *f;
+ 	f = fopen ( dep, "w" );
+	while(nodoActual != NULL){
+		fprintf(f,"\n%s\t%d\t%d",nodoActual->produ,nodoActual->canti,nodoActual->valor);
+		nodoActual = nodoActual->sig;
+	}
+	fclose ( f );	
+}
+
+
+
+
 NodoInt* agregar(char dep[15],NodoInt *lista)
 {
 	
@@ -48,15 +62,133 @@ NodoInt* agregar(char dep[15],NodoInt *lista)
 	printf("\nEscriba la cantidad inicial del producto: ");
 	scanf("%d",&cantidad);
 	lista = listaInt_ingresar(lista,cantidad,costo,nuevoP);
-	guardarArchivo(dep,cantidad,costo,nuevoP);//Aqui se colocaria una funcion que guarde el contenido de la lista en un archivo y posteriormente borre la lista.
+	guardarArchivo(dep,cantidad,costo,nuevoP);
 	return lista;
 }
 
+NodoInt* eliminar(char dep[15], NodoInt *lista)
+{
+	int eliminado;
+	lista=leerArchivo(dep,lista);
+	printf("Selecciona el producto que deseas eliminar \n");
+	imprime_productos(lista);
+	scanf("%d",&eliminado);
+	eliminado--;
+	lista=listaInt_remover(lista,eliminado);
+	reescribeArchivo(dep,lista);
+	return lista;
+}
+
+NodoInt* vender(char dep[15], NodoInt *lista)
+{
+	lista=leerArchivo(dep,lista);
+	int v,c;	//v= valor c=cantidad
+	int venta,cuantos,tmp,total;
+	char nom[15]; //guardara el nombre del producto
+	printf("Selecciona el producto que deseas vender\n");
+	imprime_productos_precio(lista);
+	scanf("%d",&venta);
+	if (venta<1)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+	venta = venta-1;
+	v=listaInt_obtenerV(lista,venta);
+	c=listaInt_obtenerC(lista,venta);
+	strcpy(nom,(listaInt_obtenerP(lista,venta,nom)));
+	printf("¿Cuantos productos deseas vender?\n");
+	scanf("%d",&cuantos);
+	if(c<cuantos)
+	{
+		printf("Lo sentimos no contamos con esa cantidad de productos\n  Contamos con : %d\n",c);
+	}
+	else
+	{
+
+	listaInt_imprimir(lista);
+	printf("El nombre del producto :%s\n",nom);
+	printf("El producto cuesta :%d\n",v);
+	printf("Tenemos disponibles %d productos\n",c);
+
+	tmp=c-cuantos;
+	total=v*cuantos;
+
+	printf("Su venta total es de %d pesos\n",total);
+	printf("quedaran disponibles  %d productos\n",tmp);
+
+	if (tmp == 0)
+	{
+		lista=listaInt_remover(lista,venta);
+		reescribeArchivo(dep,lista);
+	}
+	else
+	{
+		lista=listaInt_remover(lista,venta);
+		lista = listaInt_ingresar(lista,tmp,v,nom);
+		reescribeArchivo(dep,lista);
+	}
+	listaInt_borrar(lista);
+	}
+	
+	return lista;
+	}
+}
+
+NodoInt* comprar(char dep[15], NodoInt *lista)
+{
+	lista=leerArchivo(dep,lista);
+	int v,c;	//v= valor c=cantidad
+	int venta,cuantos,tmp,total;
+	char nom[15]; //guardara el nombre del producto
+	printf("Selecciona el producto que deseas comprar\n");
+	imprime_productos_precio(lista);
+	scanf("%d",&venta);
+	if (venta<1)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+	venta = venta-1;
+	v=listaInt_obtenerV(lista,venta);
+	c=listaInt_obtenerC(lista,venta);
+	strcpy(nom,(listaInt_obtenerP(lista,venta,nom)));
+	printf("¿Cuantos productos deseas comprar?\n");
+	scanf("%d",&cuantos);
+
+	listaInt_imprimir(lista);
+	printf("El nombre del producto :%s\n",nom);
+	printf("El producto cuesta :%d\n",v);
+	printf("Tenemos disponibles %d productos\n",c);
+
+	tmp=c+cuantos;
+	total=v*cuantos;
+
+	printf("Su compra total es de %d pesos\n",total);
+	printf("quedaran disponibles  %d productos\n",tmp);
+
+	if (tmp == 0)
+	{
+		lista=listaInt_remover(lista,venta);
+		reescribeArchivo(dep,lista);
+	}
+	else
+	{
+		lista=listaInt_remover(lista,venta);
+		lista = listaInt_ingresar(lista,tmp,v,nom);
+		reescribeArchivo(dep,lista);
+	}
+	listaInt_borrar(lista);
+	return lista;
+	}
+}
 
 int main(){
-	NodoInt *lista = NULL;
 	int opcion,depto;
 	do{
+		NodoInt *lista = NULL;
 		printf("\n\n\t Bienvenido a nuestro inventario \n\n\t Seleccione una de nuestras opciones:   ");
 		printf("\n\t1.- Ingresar un nuevo producto");
 		printf("\n\t2.- Eliminar un producto");
@@ -71,7 +203,9 @@ int main(){
 		switch (opcion)
 		{
 		case 1:
+		{
 			do{
+				NodoInt *lista = NULL;
 				printf("\n\n\t Seleccione a que departamento ingresara el nuevo producto:");
 				printf("\n\t1.- Comida");
 				printf("\n\t2.- Juguetes");
@@ -87,32 +221,215 @@ int main(){
 						char archivo[]="comida.txt";
 						lista=agregar(archivo,lista);
 						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
 						break;
 							}
 					case 2:{
 						char archivo[]="juguetes.txt";
 						lista=agregar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
 						break;
 							}
 					case 3:{
 						char archivo[]="vestimenta.txt";
-						agregar(archivo,lista);
+						lista=agregar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
 						break;
 							}
 					case 4:{
 						char archivo[]="magia.txt";
-						agregar(archivo,lista);
+						lista=agregar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
 						break;
 							}
 					case 5:{
 						char archivo[]="electronica.txt";
-						agregar(archivo,lista);
+						lista=agregar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
 						break;
 							}
 					default:
 						break;
 				}
-			}while(depto!=6);		
+			}while(depto!=6);
+
+		break;
+		}
+
+		case 2:
+		{
+			do{
+				NodoInt *lista = NULL;
+				printf("\n\n\t Seleccione en que departamento se encuentra el producto \n que desea eliminar:");
+				printf("\n\t1.- Comida");
+				printf("\n\t2.- Juguetes");
+				printf("\n\t3.- Vestimenta");
+				printf("\n\t4.- Magia");
+				printf("\n\t5.- Electronica");
+				printf("\n\t6.- Regresar al menu principal\n\t");
+				scanf("%d",&depto);
+				system("clear");
+				switch (depto)
+				{
+					case 1:{
+						char archivo[]="comida.txt";
+						lista=eliminar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 2:{
+						char archivo[]="juguetes.txt";
+						lista=eliminar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 3:{
+						char archivo[]="vestimenta.txt";
+						lista=eliminar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 4:{
+						char archivo[]="magia.txt";
+						lista=eliminar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 5:{
+						char archivo[]="electronica.txt";
+						lista=eliminar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					default:
+						break;
+				}
+			}while(depto!=6);
+		break;
+		}
+		case 3:
+		{
+			do{
+				NodoInt *lista = NULL;
+				printf("\n\n\t Seleccione en que departamento se encuentra el producto \n  que desea vender:");
+				printf("\n\t1.- Comida");
+				printf("\n\t2.- Juguetes");
+				printf("\n\t3.- Vestimenta");
+				printf("\n\t4.- Magia");
+				printf("\n\t5.- Electronica");
+				printf("\n\t6.- Regresar al menu principal\n\t");
+				scanf("%d",&depto);
+				system("clear");
+				switch (depto)
+				{
+					case 1:{
+						char archivo[]="comida.txt";
+						vender(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 2:{
+						char archivo[]="juguetes.txt";
+						vender(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 3:{
+						char archivo[]="vestimenta.txt";
+						vender(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 4:{
+						char archivo[]="magia.txt";
+						vender(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 5:{
+						char archivo[]="electronica.txt";
+						vender(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					default:
+						break;
+				}
+
+			}while(depto!=6);
+		break;
+		}
+		case 4:
+		{
+			do{
+				NodoInt *lista = NULL;
+				printf("\n\n\t Seleccione en que departamento se encuentra el producto que\n  desea Comprar:");
+				printf("\n\t1.- Comida");
+				printf("\n\t2.- Juguetes");
+				printf("\n\t3.- Vestimenta");
+				printf("\n\t4.- Magia");
+				printf("\n\t5.- Electronica");
+				printf("\n\t6.- Regresar al menu principal\n\t");
+				scanf("%d",&depto);
+				system("clear");
+				switch (depto)
+				{
+					case 1:{
+						char archivo[]="comida.txt";
+						comprar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 2:{
+						char archivo[]="juguetes.txt";
+						comprar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 3:{
+						char archivo[]="vestimenta.txt";
+						comprar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 4:{
+						char archivo[]="magia.txt";
+						comprar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					case 5:{
+						char archivo[]="electronica.txt";
+						comprar(archivo,lista);
+						listaInt_imprimir(lista);
+						listaInt_borrar(lista);
+						break;
+							}
+					default:
+						break;
+				}
+
+			}while(depto!=6);
+		break;
+		}
 		default:
 		break;
 		}
